a8c94dd02729a3f58472beec0678e015e8978b6f
Lucas Roseti <lc.roseti@gmail.com>
2024-01-23 22:23:09 -0300

    Listing and filtering tasks

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,20 @@ const database = new Database()
 
 export const routes = [
   {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { search } = req.query
+      const tasks = database.select('tasks', search ? {
+        title: search,
+        description: search,
+      } : null)
+      return res
+        .setHeader('Content-Type', 'application/json')	
+        .end(JSON.stringify(tasks))
+    }
+  },
+  {
     method: 'POST',
     path: buildRoutePath('/tasks'),
     handler: (req, res) => {
