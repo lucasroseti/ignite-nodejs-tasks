9b332bbdbf55669521d330f0e3ef1d16c8ae2e0c
Lucas Roseti <lc.roseti@gmail.com>
2024-01-24 23:20:09 -0300

    Improvements in import csv file

--- src/utils/read-csv-records.js
+++ src/utils/read-csv-records.js
@@ -4,34 +4,31 @@ import { parse } from 'csv-parse'
 const __dirname = new URL('../../', import.meta.url).pathname.substring(1)
 
 const processFile = async () => {
-  const records = [];
-  const parser = fs
+  return fs
     .createReadStream(`${__dirname}/tasks.csv`)
-    .pipe(parse())
-  for await (const record of parser) {
-    records.push(record);
-  }
-  return records;
+    .pipe(parse({
+      delimiter: ',',
+      skipEmptyLines: true,
+      fromLine: 2
+    }))
 }
 
 (async () => {
   const records = await processFile()
 
-  for await (const [key, record] of records.entries()) {
-    if (key > 0) {
-      const [title, description] = record
-      const task = {
-        title,
-        description
-      }
-
-      await fetch('http://localhost:3333/tasks', {
-        method: 'POST',
-        body: JSON.stringify(task),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
+  for await (const record of records) {
+    const [title, description] = record
+    const task = {
+      title,
+      description
     }
+
+    await fetch('http://localhost:3333/tasks', {
+      method: 'POST',
+      body: JSON.stringify(task),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
   }
 })()
